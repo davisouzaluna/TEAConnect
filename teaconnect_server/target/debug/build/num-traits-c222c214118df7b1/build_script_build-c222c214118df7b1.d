87a938a74a884bf14119876a30da59fb5abe66a7
Davi Luna <Sdavi738@gmail.com>
2024-09-08 19:19:13 -0300

    adding gitignore to rust project

--- teaconnect_server/target/debug/build/num-traits-c222c214118df7b1/build_script_build-c222c214118df7b1.d
+++ teaconnect_server/target/debug/build/num-traits-c222c214118df7b1/build_script_build-c222c214118df7b1.d
@@ -1,5 +1,5 @@
-/home/davi/TEAConnect/TEAConnect_server/target/debug/build/num-traits-c222c214118df7b1/build_script_build-c222c214118df7b1: /home/davi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs
+/home/davi/TEAConnect/teaconnect_server/target/debug/build/num-traits-c222c214118df7b1/build_script_build-c222c214118df7b1: /home/davi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs
 
-/home/davi/TEAConnect/TEAConnect_server/target/debug/build/num-traits-c222c214118df7b1/build_script_build-c222c214118df7b1.d: /home/davi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs
+/home/davi/TEAConnect/teaconnect_server/target/debug/build/num-traits-c222c214118df7b1/build_script_build-c222c214118df7b1.d: /home/davi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs
 
 /home/davi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs:
